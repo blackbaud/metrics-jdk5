f9d189bc83cf5c4a722bacdc712ee2955a123398
mrupe <mrupe@mrupe2.blackbaud.global>
2013-02-26 15:05:57 -0600

    Backported to work with Java 1.5

---
+++ metrics-core-jdk5/src/test/java/com/yammer/metrics/examples/ExampleRunner.java
@@ -0,0 +1,52 @@
+package com.yammer.metrics.examples;
+
+import com.yammer.metrics.Metrics;
+import com.yammer.metrics.core.Counter;
+import com.yammer.metrics.core.Histogram;
+import com.yammer.metrics.reporting.ConsoleReporter;
+
+import java.io.File;
+import java.util.List;
+import java.util.concurrent.*;
+
+public class ExampleRunner {
+    private static final int WORKER_COUNT = 10;
+    private static final BlockingQueue<File> JOBS = new LinkedBlockingQueue<File>();
+    private static final ExecutorService POOL = Executors.newFixedThreadPool(WORKER_COUNT);
+    private static final Counter QUEUE_DEPTH = Metrics.newCounter(ExampleRunner.class, "queue-depth");
+    private static final Histogram DIRECTORY_SIZE = Metrics.newHistogram(ExampleRunner.class, "directory-size", false);
+
+    public static class Job implements Runnable {
+        public void run() {
+            try {
+                while (true) {
+                    final File file = JOBS.poll(60, TimeUnit.SECONDS);
+                    QUEUE_DEPTH.dec();
+                    if (file.isDirectory()) {
+                        final List<File> contents = new DirectoryLister(file).list();
+                        DIRECTORY_SIZE.update(contents.size());
+                        QUEUE_DEPTH.inc(contents.size());
+                        JOBS.addAll(contents);
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+    public static void main(String[] args) throws Exception {
+        ConsoleReporter.enable(10, TimeUnit.SECONDS);
+
+        System.err.println("Scanning all files on your hard drive...");
+
+        JOBS.add(new File("/"));
+        QUEUE_DEPTH.inc();
+        for (int i = 0; i < WORKER_COUNT; i++) {
+            POOL.submit(new Job());
+        }
+
+        POOL.awaitTermination(10*24*60*60, TimeUnit.SECONDS);
+    }
+}
---
+++ metrics-spring-jdk5/src/test/java/com/yammer/metrics/spring/MeteredInterfaceImpl.java
@@ -0,0 +1,20 @@
+package com.yammer.metrics.spring;
+
+public class MeteredInterfaceImpl implements MeteredInterface {
+
+	
+	public boolean timedMethod() {
+		return true;
+	}
+
+	
+	public boolean meteredMethod() {
+		return true;
+	}
+
+	
+	public void exceptionMeteredMethod() throws Throwable {
+		throw new BogusException();
+	}
+
+}
